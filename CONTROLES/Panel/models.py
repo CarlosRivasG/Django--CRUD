208d3abb46f4750a0534afdf50512b867e47f7f8
CarlosR <cmrivas20@gmail.com>
2022-12-17 21:55:43 -0300

    CRUD listo

--- CONTROLES/Panel/models.py
+++ CONTROLES/Panel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Usuarios(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=30, null=False)
+    apellido = models.CharField(max_length=30, null=False)
+    correo = models.CharField(max_length=50, null=False)
+    telefono = models.IntegerField(null=False)
+    f_nacimiento = models.DateField(null=False)
+    f_registro = models.DateTimeField(auto_now_add=True, null=True)
+    class Meta:
+        db_table ='usuarios'
--- CONTROLES/Panel/views.py
+++ CONTROLES/Panel/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .models import Usuarios
 
 TEMPLATE_DIRS = (
     'os.path.join(BASE_DIR, "templates"),'
@@ -10,13 +10,51 @@ def index(request):
     return render(request,'index.html')
 
 def listar(request):
-    return render(request,'crud_usuarios/listar.html')
+    users = Usuarios.objects.all()
+    datos = { 'usuarios' : users}
+    return render(request,'crud_usuarios/listar.html', datos)
 
 def agregar(request):
-    return render(request,'crud_usuarios/agregar.html')
-
-def eliminar(request):
-    return render(request,'crud_usuarios/eliminar.html')
+    if request.method=='POST':
+        if request.POST.get('nombre') and request.POST.get('apellido') and request.POST.get('correo') and request.POST.get('telefono') and request.POST.get('f_nacimiento'):
+            user = Usuarios()
+            user.nombre = request.POST.get('nombre')
+            user.apellido = request.POST.get('apellido')
+            user.correo = request.POST.get('correo')
+            user.telefono = request.POST.get('telefono')
+            user.f_nacimiento = request.POST.get('f_nacimiento')
+            user.save()
+            return redirect('listar')
+    else:
+        return render(request,'crud_usuarios/agregar.html')
 
 def actualizar(request):
-    return render(request,'crud_usuarios/actualizar.html')
+    if request.method=='POST':
+        if request.POST.get('id') and request.POST.get('nombre') and request.POST.get('apellido') and request.POST.get('correo') and request.POST.get('telefono') and request.POST.get('f_nacimiento'):
+            user = Usuarios()
+            user.id = request.POST.get('id')
+            user.nombre = request.POST.get('nombre')
+            user.apellido = request.POST.get('apellido')
+            user.correo = request.POST.get('correo')
+            user.telefono = request.POST.get('telefono')
+            user.f_nacimiento = request.POST.get('f_nacimiento')
+            user.save()
+            return redirect('listar')
+
+    else:
+        users = Usuarios.objects.all()
+        datos = { 'usuarios' : users}
+        return render(request,'crud_usuarios/actualizar.html', datos)        
+
+def eliminar(request):
+    if request.method=='POST':
+        if request.POST.get('id'):
+            id_a_borrar = request.POST.get('id')
+            tupla = Usuarios.objects.get(id = id_a_borrar)
+            tupla.delete()
+            return redirect('listar')
+    else:
+            users = Usuarios.objects.all()
+            datos = { 'usuarios' : users}
+            return render(request,'crud_usuarios/eliminar.html', datos)
+
